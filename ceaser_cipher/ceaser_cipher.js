4f94f4362663dde33613d6a4ba220e19d5caddaa
Cybergman-droid <jasongakpo20@gmail.com>
2025-07-02 13:08:04 +0100

    Add basic structure for Caesar cipher UI and JS
    Added IDs to input and output textareas in the HTML for easier DOM access. Created initial JavaScript file with character set definition and variable declarations to support Caesar cipher functionality.

---
+++ ceaser_cipher/ceaser_cipher.js
@@ -0,0 +1,20 @@
+const charSet = [
+    // Uppercase letters
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+    'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+    // Lowercase letters
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+    // Digits
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+    // Punctuation symbols
+    '!', '\"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~'
+];
+let cipherSet = charSet;
+
+let plainText 
+let encryptedText
+let key 
+
+
+
